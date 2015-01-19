86161320659226484f04bd3b1362ffcb61f08bed
Draedixe <alexis.margottin@gmail.com>
2015-01-19 20:03:49 +0100

    Possibilité de créer une compo (justes roles pour l'instant), manque securité coté serveur

--- mafia/src/Mafia/RolesBundle/Controller/CompositionController.php
+++ mafia/src/Mafia/RolesBundle/Controller/CompositionController.php
@@ -9,6 +9,7 @@
 namespace Mafia\RolesBundle\Controller;
 
 
+use Mafia\RolesBundle\Entity\Composition;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CompositionController extends Controller{
@@ -27,4 +28,26 @@ class CompositionController extends Controller{
         ));
     }
 
+    public function ajoutCompositionAction()
+    {
+        $newComposition = new Composition();
+        $request = $this->get('request');
+        $composition = $request->get("composition");
+
+        $repository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('MafiaRolesBundle:Role');
+        $newComposition->setNomCompo("Test");
+        $newComposition->setOfficielle(false);
+        foreach($composition as $idRole)
+        {
+            $role = $repository->find($idRole);
+            $newComposition->addRoleCompo($role);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($newComposition);
+        $em->flush();
+        return $this->redirect($this->generateUrl('vue_role', array('id'=>1)));
+
+    }
 } 
