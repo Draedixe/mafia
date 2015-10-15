41dfd10bb243f003441b87a211de66b0b06be2b1
Draedixe <alexis.margottin@gmail.com>
2015-10-15 18:37:12 +0200

    Ajout du statut Tué

--- mafia/src/Mafia/PartieBundle/Entity/StatutEnum.php
+++ mafia/src/Mafia/PartieBundle/Entity/StatutEnum.php
@@ -13,8 +13,9 @@ abstract class StatusEnum {
     const PIEGE = 5;
     const BLOQUE = 6;
     const EMPRISONNE = 7;
+    const TUE = 8;
 
-    private static $statutPartie = array('PROTEGE' => 0, 'CONTROLE' => 1, 'ESSENCE' => 2, 'SAUVE' => 3, 'ECHANGE' => 4, 'PIEGE' => 5, 'BLOQUE' => 6, 'EMPRISONNE' => 7);
+    private static $statutPartie = array('PROTEGE' => 0, 'CONTROLE' => 1, 'ESSENCE' => 2, 'SAUVE' => 3, 'ECHANGE' => 4, 'PIEGE' => 5, 'BLOQUE' => 6, 'EMPRISONNE' => 7, 'TUE' => 8);
 
     public static function getStatusPartie()
     {
