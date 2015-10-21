dbb01aef69632697ebff25cdf42ef45f9de6561c
Draedixe <alexis.margottin@gmail.com>
2015-10-21 11:18:11 +0200

    Développement du Pyromane et de ses 2 actions possibles

--- mafia/src/Mafia/RolesBundle/Controller/NeutreController.php
+++ mafia/src/Mafia/RolesBundle/Controller/NeutreController.php
@@ -20,14 +20,12 @@ class NeutreController extends Controller {
         /* Récupérons un max de repositories ! */
         $repositoryUser = $this->getDoctrine()
             ->getManager()
-            ->getRepository('MafiaRolesBundle:User');
+            ->getRepository('MafiaPartieBundle:UserPartie');
 
-        $cible = $repositoryUser->find($idCible);
+        $ciblePartieCourant = $repositoryUser->find($idCible);
 
         $userGlobal = $this->getUser();
         $userPartieCourant = $userGlobal->getUserCourant();
-
-        $ciblePartieCourant = $cible->getUserCourant();
 
         /* Si les 2 joueurs sont en jeu */
         if($userPartieCourant != null && $ciblePartieCourant != null){
@@ -58,7 +56,97 @@ class NeutreController extends Controller {
                 }
             }
         }
-
-
     }
+
+    public function pyromaneArroseAction($idCible){
+
+        /* Récupérons un max de repositories ! */
+        $repositoryUser = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('MafiaPartieBundle:UserPartie');
+
+        $ciblePartieCourant = $repositoryUser->find($idCible);
+
+        $userGlobal = $this->getUser();
+        $userPartieCourant = $userGlobal->getUserCourant();
+
+        /* Si les 2 joueurs sont en jeu */
+        if($userPartieCourant != null && $ciblePartieCourant != null){
+            $partie = $userPartieCourant->getPartie();
+            $partieCible = $ciblePartieCourant->getPartie();
+
+            /* On vérifie que les parties existent bien */
+            if($partie != null && $partieCible != null){
+
+                /* Si ils sont dans la même partie */
+                if($partie == $partieCible){
+
+                    /* S'ils sont bien vivants tous les deux */
+                    if($userPartieCourant->getVivant() && $ciblePartieCourant->getVivant()){
+
+                        /* Si le joueur a bien un rôle */
+                        if($userPartieCourant->getRole() != null){
+
+                            /* Si le joueur est bien Pyromane */
+                            if($userPartieCourant->getRole()->getEnumRole() == RolesEnum::PYROMANE){
+                                $statutEssence = new Statut(StatusEnum::ESSENCE,$ciblePartieCourant,$userPartieCourant);
+                                $ciblePartieCourant->setEssencePar($userPartieCourant->getId());
+                                $em = $this->getDoctrine()->getManager();
+                                $em->persist($statutEssence);
+                                $em->persist($ciblePartieCourant);
+                                $em->flush();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public function pyromaneAllumeAction(){
+
+        /* Récupérons un max de repositories ! */
+        $repositoryUser = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('MafiaPartieBundle:UserPartie');
+
+
+        $userGlobal = $this->getUser();
+        $userPartieCourant = $userGlobal->getUserCourant();
+
+        /* Si les 2 joueurs sont en jeu */
+        if($userPartieCourant != null){
+            $partie = $userPartieCourant->getPartie();
+
+            /* On vérifie que les parties existent bien */
+            if($partie != null){
+
+                    /* S'ils sont bien vivants tous les deux */
+                    if($userPartieCourant->getVivant()){
+
+                        /* Si le joueur a bien un rôle */
+                        if($userPartieCourant->getRole() != null){
+
+                            /* Si le joueur est bien Pyromane */
+                            if($userPartieCourant->getRole()->getEnumRole() == RolesEnum::PYROMANE){
+                                $em = $this->getDoctrine()->getManager();
+                                $query = $em->createQuery('SELECT u FROM Mafia\PartieBundle\Entity\UserPartie u,Mafia\PartieBundle\Entity\Statut s WHERE u.partie. = :idPartie AND u.vivant = true AND s.victime = u.id AND s.acteur = :idActeur AND s.enumStatut = :enumStatut');
+                                $query->setParameters(array(
+                                    'idPartie' => $partie->getId(),
+                                    'idActeur' => $userPartieCourant,
+                                    'enumStatut' => StatusEnum::ESSENCE
+                                ));
+                                $cibles = $query->getResult();
+                                foreach ($cibles as $ciblePartieCourant){
+                                    $em->persist(new Statut(StatusEnum::TUE,$ciblePartieCourant,$userPartieCourant));
+                                }
+
+                                $em->flush();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
 } 
