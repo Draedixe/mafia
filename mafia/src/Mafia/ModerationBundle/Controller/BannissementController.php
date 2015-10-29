a953027e6c6cbdae80439ad5484c8b6984b9a98c
Draedixe <alexis.margottin@gmail.com>
2015-10-29 20:00:56 +0100

    Vues bannissements

--- mafia/src/Mafia/ModerationBundle/Controller/BannissementController.php
+++ mafia/src/Mafia/ModerationBundle/Controller/BannissementController.php
@@ -3,11 +3,79 @@
 namespace Mafia\ModerationBundle\Controller;
 
 use Mafia\ModerationBundle\Entity\Bannissement;
+use Mafia\UserBundle\Entity\regroupementVariable;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class BannissementController extends Controller
 {
 
+    public function listeBannissementsAction($page){
+        $repositoryBans = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('MafiaModerationBundle:Bannissement');
+        $bans = $repositoryBans->findAll();
 
+        $nbBans = count($bans);
+        if($nbBans%regroupementVariable::NB_BANS_PAR_PAGE_TOUT > 0){
+            $nbPages = floor($nbBans/regroupementVariable::NB_BANS_PAR_PAGE_TOUT) +1;
+        }
+        else{
+            $nbPages = $nbBans/regroupementVariable::NB_BANS_PAR_PAGE_TOUT;
+        }
+        if($nbPages == 1){
+            return $this->render('MafiaModerationBundle:Affichages:liste_bans.html.twig', array(
+                'bans' => $bans,
+                'pageCourante' => $page,
+                'nbPages' => $nbPages
+            ));
+        }
+        else{
+            $bansSurPage = array_slice($bans,( regroupementVariable::NB_BANS_PAR_PAGE_TOUT * ($page-1)),regroupementVariable::NB_BANS_PAR_PAGE_TOUT);
+            return $this->render('MafiaModerationBundle:Affichages:liste_bans.html.twig', array(
+                'bans' => $bansSurPage,
+                'pageCourante' => $page,
+                'nbPages' => $nbPages
+            ));
+        }
+
+    }
+
+    public function listeBannissementsUserAction($id,$page){
+        $repositoryUser = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('MafiaUserBundle:User');
+        $user = $repositoryUser->find($id);
+
+        $repositoryBans = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('MafiaModerationBundle:Bannissement');
+
+        $bans = $repositoryBans->findBy(array("userBanni" => $user));
+        $nbBans = count($bans);
+        if($nbBans%regroupementVariable::NB_BANS_PAR_PAGE_USER > 0){
+            $nbPages = floor($nbBans/regroupementVariable::NB_BANS_PAR_PAGE_USER) +1;
+        }
+        else{
+            $nbPages = $nbBans/regroupementVariable::NB_BANS_PAR_PAGE_USER;
+        }
+        if($nbPages == 1){
+            return $this->render('MafiaModerationBundle:Affichages:liste_bans_user.html.twig', array(
+                'user' => $user,
+                'bans' => $bans,
+                'pageCourante' => $page,
+                'nbPages' => $nbPages
+            ));
+        }
+        else{
+            $bansSurPage = array_slice($bans,( regroupementVariable::NB_BANS_PAR_PAGE_USER * ($page-1)),regroupementVariable::NB_BANS_PAR_PAGE_USER);
+            return $this->render('MafiaModerationBundle:Affichages:liste_bans_user.html.twig', array(
+                'user' => $user,
+                'bans' => $bansSurPage,
+                'pageCourante' => $page,
+                'nbPages' => $nbPages
+            ));
+        }
+
+    }
 }
--- mafia/src/Mafia/UserBundle/Entity/regroupementVariable.php
+++ mafia/src/Mafia/UserBundle/Entity/regroupementVariable.php
@@ -11,4 +11,8 @@ abstract class regroupementVariable {
 
     /* Nombre de contact dont les MP sont affichés par page */
     const NB_MP_PAR_PAGE = 1;
+    /* Nbr de bannissement sur la page des bans de tel user */
+    const NB_BANS_PAR_PAGE_USER = 1;
+    /* Nbr de bannissement sur la page de tous les bans*/
+    const NB_BANS_PAR_PAGE_TOUT = 1;
 } 
