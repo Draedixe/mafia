133639b6061b5801585171acd25439e2ea0f1028
Draedixe <alexis.margottin@gmail.com>
2015-11-17 18:44:20 +0100

    Mise en place des enum pour les erreur etc ...

---
+++ mafia/src/Mafia/RolesBundle/Entity/Enums/MessageEnum.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 12/11/2015
+ * Time: 22:13
+ */
+
+class MessageEnum {
+
+    // TYPEMESSAGE_entité_raison
+
+    const ERREUR_role_non_existant = "Ce rôle n'existe pas";
+    const ERREUR_role_unique = "Ce rôle doit être unique";
+    const ERREUR_composition_non_existant = "Cette composition n'existe pas";
+    const ERREUR_composition_non_createur = "Vous n'êtes pas le créateur de cette composition";
+}
